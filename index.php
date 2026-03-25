b2bc75b8e7838d5362bfad37181c95e935541b81
adityayadav176 <ay6598aditya@gmail.com>
2026-03-25 21:17:05 +0530

    my first php class

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my website</title>
+</head>
+<body>
+    <div class="container">
+        this is my first php website
+    </div>
+    <?php
+    echo "this is printed using php";
+
+    $variable1 = 34;
+    $variable2 = 42;
+    echo $variable1;
+    echo $variable2;
+
+    Echo $variable1 + $variable2;
+
+    // arithmetic operator 
+    echo "<br>";
+    echo "The value of variable1 and + variable2 ";
+    echo "<br>";
+    echo $variable1 + $variable2;
+    echo "<br>";
+    echo "The value of variable1 and - variable2 ";
+    echo "<br>";
+    echo $variable1 - $variable2;
+    echo "<br>";
+    echo "The value of variable1 and * variable2 ";
+    echo "<br>";
+    echo $variable1 * $variable2;
+    echo "<br>";
+    echo "The value of variable1 and / variable2 ";
+    echo "<br>";
+    echo $variable1 / $variable2;
+    echo "<br>";
+    echo "The value of variable1 and % variable2 ";
+    echo "<br>";
+    echo $variable1 % $variable2;
+    // assignment operator
+    // comparison operator
+    //increment/ decrement operator 
+    //logical operator
+
+    ?>
+</body>
+</html>
