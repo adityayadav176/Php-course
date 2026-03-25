6f7f4c8af85f2bc3b9d7c5033c749812b9df48e9
adityayadav176 <ay6598aditya@gmail.com>
2026-03-25 21:55:24 +0530

    php variables and operators

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
     <?php
     echo "this is printed using php";
 
-    $variable1 = 34;
-    $variable2 = 42;
+    $variable1 = 5;
+    $variable2 = 2;
     echo $variable1;
     echo $variable2;
 
@@ -40,10 +40,80 @@
     echo "The value of variable1 and % variable2 ";
     echo "<br>";
     echo $variable1 % $variable2;
+    echo "<br>";
     // assignment operator
+
+    $newVar = $variable1;
+    // $newVar += 1;
+    // $newVar *= 2;
+    // $newVar -= 1;
+    // $newVar /= 2;
+    echo "the value of new var is";
+    echo "<br>";
+    echo $newVar;
+   
+
     // comparison operator
+    echo "<br>";
+    echo "the value of 1 == 4 is";
+    echo var_dump(1==4);
+
+    echo "<br>";
+    echo "the value of 1 != 4 is";
+    echo var_dump(1!=4);
+    echo "<br>";
+    echo "the value of 1 >= 4 is";
+    echo var_dump(1>=4);
+    echo "<br>";
+    echo "the value of 1 <= 4 is";
+    echo var_dump(1<=4);
+    
     //increment/ decrement operator 
+    echo "<br>";
+    // echo $variable1++;
+    // echo "<br>";
+    // echo $variable1;
+
+    // echo $variable1--;
+    // echo "<br>";
+    // echo $variable1
+
+    // echo ++$variable1;
+    // echo "<br>";
+    // echo $variable1
+    echo --$variable1;
+    echo "<br>";
+    echo $variable1;
+    echo "<br>";
     //logical operator
+
+    // and (&&)
+    // if both conditions are true then return true
+    $myVar = (false and true);
+    echo $myVar;
+    echo "<br>";
+     echo var_dump($myVar);
+
+    // or (||)
+    // if one condition are true
+    $myVar2 = (true or false);
+    echo $myVar2;
+    echo "<br>";
+     echo var_dump($myVar2);
+
+    // xor 
+
+    $myVar3 = (true xor false);
+    echo $myVar3;
+    echo "<br>";
+     echo var_dump($myVar3);
+    // !
+
+    $myVar4 = !(true);
+    echo $myVar4;
+    echo "<br>";
+    echo var_dump($myVar4);
+
 
     ?>
 </body>
